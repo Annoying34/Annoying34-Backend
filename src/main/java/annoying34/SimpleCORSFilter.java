891f67dddef1cad1e9a157262d4b212b836cee06
Konstantin Deichmann <k.deichmann@mac.com>
2017-07-06 09:59:35 +0200

    update Allowed Header

--- src/main/java/annoying34/SimpleCORSFilter.java
+++ src/main/java/annoying34/SimpleCORSFilter.java
@@ -28,7 +28,7 @@ public class SimpleCORSFilter implements Filter {
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me, email, password, imapURL, smtpUrl");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me, email, password, imapURL, smtpUrl, name");
 
         chain.doFilter(req, res);
     }
